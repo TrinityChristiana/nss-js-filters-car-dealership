c0b22daf0d7dfbe4cd686cda43c90834db27f257
Trinity Terry <trinitycterry@gmail.com>
2020-01-24 15:51:37 -0600

    found popular models

--- src/scripts/findPopularModel.js
+++ src/scripts/findPopularModel.js
@@ -1,7 +1,12 @@
-const findPopularModel = {
-    run(){
-        return "empty";
+import method from "./globalMethods.js";
 
+const findPopularModel = {
+    run(data){
+        const models = method.getSelector(data, ["vehicle", "model"]);
+        const duplicates = method.countDuplicates(models);
+        const sortedObj = method.sortObjFromGtoL(duplicates);
+        console.log(sortedObj);
+        return method.collectHighestValues(sortedObj);
     }
 }
 
