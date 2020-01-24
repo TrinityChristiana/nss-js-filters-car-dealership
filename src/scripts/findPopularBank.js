570048f0e7f8f5e55b0827116d9a565872e14b62
Trinity Terry <trinitycterry@gmail.com>
2020-01-24 15:54:02 -0600

    found popular bank

--- src/scripts/findPopularBank.js
+++ src/scripts/findPopularBank.js
@@ -1,7 +1,11 @@
-const findPopularBank = {
-    run(){
-        return "empty";
+import method from "./globalMethods.js";
 
+const findPopularBank = {
+    run(data) {
+        const banks = method.getSelector(data, ["credit", "credit_provider"]);
+        const duplicates = method.countDuplicates(banks);
+        const sortedObj = method.sortObjFromGtoL(duplicates);
+        return method.collectHighestValues(sortedObj);
     }
 }
 
