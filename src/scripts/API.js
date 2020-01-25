c5dacc46ef7db7b40ed10d201bfc7dc9b75b595b
Trinity Terry <trinitycterry@gmail.com>
2020-01-25 12:04:53 -0600

    added comments

--- src/scripts/API.js
+++ src/scripts/API.js
@@ -1,7 +1,14 @@
+
+import carObj from "./fakeapi.js"
+
 const API = {
     fetchData() {
         return fetch("http://localhost:8088/cars")
-            .then(resp => resp.json());
+            .then(resp => resp.json())
+            // if json server not availible use default object as data from ./fakeapi.js
+            .catch(() => {
+                return carObj;
+              });
     }
 }
 
